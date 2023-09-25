c3ed80c4920371650e6ad112c6faf7eb8b59185b
beksultan <hfjcgyrishwidhzhd1038@gmail.com>
2023-09-25 20:03:46 +0600

    add slider for service page and adding fonts

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,11 +1,40 @@
-
+import scss from './Services.module.scss'
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import Slider from '../../components/ServicesPage/Slider/Slider';
 
 const Services = () => {
-    return (
-        <div>
-<h1>Наши услуги</h1>
-        </div>
-    );
+  const sliderContent = [
+    {
+      img: './src/components/ServicesPage/img/portfolioImg_services-page.png',
+      title: 'ОшТв (сайт)',
+      desc: 'Редизайн популярного канала в городе Ош'
+    },
+    {
+      img: './src/components/ServicesPage/img/portfolioImg_services-page.png',
+      title: 'ОшТв (сайт)',
+      desc: 'Редизайн популярного канала в городе Ош'
+    },
+    {
+      img: './src/components/ServicesPage/img/portfolioImg_services-page.png',
+      title: 'ОшТв (сайт)',
+      desc: 'Редизайн популярного канала в городе Ош'
+    },
+    {
+      img: './src/components/ServicesPage/img/portfolioImg_services-page.png',
+      title: 'ОшТв (сайт)',
+      desc: 'Редизайн популярного канала в городе Ош'
+    },
+  ]
+  return (
+    <div className={scss.Services}>
+      <Header />
+      <section className={`size-1200 ${scss.Services__slider}`}>
+        <Slider children={sliderContent} />
+      </section>
+      {/*<Footer />*/}
+    </div>
+  );
 }
 
 export default Services;
