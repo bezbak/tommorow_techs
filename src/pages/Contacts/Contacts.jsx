cd3ba33928c2869fd143f11e727f5d894cdbd358
Savitar55555 <157879633+Savitar55555@users.noreply.github.com>
2024-05-27 21:03:57 +0600

    correct contact page

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -10,12 +10,13 @@ import faceIcon from '../../components/img/Facebook-icon.png'
 const Contacts = () => {
     return (
         <div>
+            <br />
             <Header />
             <div className={scss.block}>
                 <div className={scss.Image}>
                 </div>
                 <div>
-                    <div>
+                    <div className={scss.text2}>
                         <div  className={scss.text1}>
                             <h1>Контакты</h1>
                             <p>Мы открыты для сотрудничества с вами <br /> и готовы обсудить ваш будуший проект</p>
@@ -26,16 +27,16 @@ const Contacts = () => {
                                 <a href="">projectnew@tt.io</a><br />
                                 <a href="tel:+996700121212">+996(700)12-12-12</a>
                             </div>
-                            <div>
+                            <div className={scss.wrap}>
                                 <p>Адрес</p>
                                 <a href="https://www.google.com/maps/place/72+%D0%9F%D1%80.+%D0%9C%D0%B0%D1%81%D0%B0%D0%BB%D0%B8%D0%B5%D0%B2%D0%B0,+%D0%9E%D1%88/@40.529333,72.802328,17z/data=!3m1!4b1!4m6!3m5!1s0x38bdaea77996f4ad:0x84ded06c3db376e5!8m2!3d40.529329!4d72.8049029!16s%2Fg%2F11c4sm82wt?entry=ttu">Кыргызстан <br></br> город Ош,Масалиева 72</a>
                             </div>
-                            <div>
+                            <div className={scss.wrap}>
                                 <p>Контакты</p>
                                 <a href="tel:+996700121212">+996(700)12-12-12</a><br />
                                 <a href="tel:+996700121212">+996(700)12-12-12</a>
                             </div>
-                            <div className="links">
+                            <div className={scss.wrap}>
                                 <p>Социалные сети</p>
                                 <a className={scss.links} href="https://t.me/amk2k">
                                     <img className={scss.icons} src={tgIcon} alt="tg" />
@@ -51,7 +52,7 @@ const Contacts = () => {
                                 </a>
         
                             </div>
-                            <div>
+                            <div className={scss.wrap}>
                                 <p>Карьера</p>
                                 <a href="jobsemail@tt.io">jobsemail@tt.io</a>
                             </div>
