1390b37028f091e76c1c14c19f8e2be88fcd3ef7
TIMMUZY <tynchtykergesh02@gmail.com>
2024-07-01 01:47:24 +0600

    ref: fix Project

--- src/components/ServicesPage/Feedback/Feedback.jsx
+++ src/components/ServicesPage/Feedback/Feedback.jsx
@@ -3,16 +3,19 @@ import scss from './Feedback.module.scss'
 
 const Feedback = () => {
   return (
+    <>
     <div className={scss.Feedback}>
       <div>
         <div>
-          <img src="./src/components/ServicesPage/img/avatar.png" alt="" />
+          <img src="./src/components/ServicesPage/img/avatarik.jpeg" alt="avatar" />
           <p>Мухаммад /CEO</p>
         </div>
         <h3>Мы верим в силу инноваций и готовы с вами идти в ногу с быстро меняющимся миром бизнеса. Давайте вместе делать ваш бизнес более успешным и готовым к вызовам будущего.</h3>
       </div>
-      <img src="./src/components/ServicesPage/img/itudyr.png" alt="" />
+      <img src="./src/components/ServicesPage/img/itudyr.png" alt="itudyr" />
     </div>
+    <div className={scss.line}></div>
+    </>
   )
 }
 
