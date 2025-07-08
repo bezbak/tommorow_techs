dc7b61b8b5b05702c8dc9df97ed62c8edb7dfabc
bezbak <Cemetey.2019@gmail.com>
2025-07-08 08:00:22 +0600

    test

--- src/components/ServicesPage/Feedback/Feedback.jsx
+++ src/components/ServicesPage/Feedback/Feedback.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import scss from './Feedback.module.scss'
-
+import img from '/src/components/ServicesPage/img/avatarik.jpeg'
+import img2 from '/src/components/ServicesPage/img/itudyr.png'
 const Feedback = () => {
   return (
     <>
     <div className={scss.Feedback}>
       <div>
         <div>
-          <img src="./src/components/ServicesPage/img/avatarik.jpeg" alt="avatar" />
+          <img src={img} alt="avatar" />
           <p>Мухаммад /CEO</p>
         </div>
         <h3>Мы верим в силу инноваций и готовы с вами идти в ногу с быстро меняющимся миром бизнеса. Давайте вместе делать ваш бизнес более успешным и готовым к вызовам будущего.</h3>
       </div>
-      <img src="./src/components/ServicesPage/img/itudyr.png" alt="itudyr" />
+      <img src={img2} alt="itudyr" />
     </div>
     <div className={scss.line}></div>
     </>
