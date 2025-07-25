65124e1cd6ad110374d58f63e784bb40c6eca450
bezbak <Cemetey.2019@gmail.com>
2025-07-25 06:51:02 +0600

    add new animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 import MainRouter from "./routes/MainRouter"
 import { useRef, useLayoutEffect, useState } from "react";
-// import { gsap } from "gsap-trial";
-// import { ScrollTrigger } from "gsap-trial/ScrollTrigger";
-// import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import { gsap } from "gsap-trial";
+import { ScrollTrigger } from "gsap-trial/ScrollTrigger";
+import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
 import './i18n';
 import Preloader from "./components/Preloader/Preloader";
 import ScrollToTopButton from "./components/ScrollToTopButton/ScrollToTopButton";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
-// gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 function App() {
   const [isLoading, setIsLoading] = useState(true);
-  // useLayoutEffect(() => {
-  //   ScrollSmoother.create({
-  //     smooth: 2 // how long (in seconds) it takes to "catch up" to the native scroll position
-  //   });
-  // }, []);
+  useLayoutEffect(() => {
+    ScrollSmoother.create({
+      smooth: 2 // how long (in seconds) it takes to "catch up" to the native scroll position
+    });
+  }, []);
   return (
     <>
       {isLoading && <Preloader setIsLoading={setIsLoading} />}
-      {/* <div id="smooth-wrapper" className="App"> */}
+      <div id="smooth-wrapper" className="App">
         <ScrollToTop />
 
-        {/* <div id="smooth-content"> */}
+        <div id="smooth-content">
           <MainRouter isLoading={isLoading} />
           <ScrollToTopButton />
-        {/* </div>
-      </div> */}
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Portfolio/Portfolio2.jsx
+++ src/components/Portfolio/Portfolio2.jsx
@@ -1,71 +1,104 @@
 import React, { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
 import img_1 from '../img/portfolioImg_1.png';
 import img_2 from '../img/portfolioImg_2.png';
 import img_3 from '../img/portfolioImg_3.png';
 import classes from './Portfolio2.module.scss';
 
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
 const Portfolio2 = () => {
-  const portfolioRef = useRef(null);
+  const wrapperRef = useRef(null);
+  const contentRef = useRef(null);
 
   useEffect(() => {
-    const handleScroll = () => {
-      const elements = portfolioRef.current.querySelectorAll(`.${classes.portfolio_item}`);
-      elements.forEach(el => {
-        const rect = el.getBoundingClientRect();
-        const isVisible = rect.top < window.innerHeight * 0.8 && rect.bottom >= 0;
-        el.classList.toggle(classes.visible, isVisible);
-      });
-    };
+    const smoother = ScrollSmoother.create({
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+      smooth: 1.5,
+      effects: true,
+    });
 
-    window.addEventListener('scroll', handleScroll);
-    handleScroll(); // Initial check
-    
-    return () => window.removeEventListener('scroll', handleScroll);
+    const content = contentRef.current;
+    const contentWidth = content.scrollWidth;
+    const windowWidth = window.innerWidth;
+    const scrollDistance = (contentWidth+400) - windowWidth;
+
+    gsap.to(content, {
+      x: -scrollDistance,
+      ease: "none",
+      scrollTrigger: {
+        trigger: wrapperRef.current,
+        start: "top top",
+        end: () => `+=${scrollDistance}`,
+        scrub: true,
+        pin: true,
+        anticipatePin: 1,
+        invalidateOnRefresh: true,
+        markers:true
+      }
+    });
   }, []);
 
   return (
     <div className={classes.portfolio_wrapper}>
-      {/* <div className={classes.header}>
-        <h2 className={classes.subtitle}>Bullets to visuals in a click.</h2>
-        <p className={classes.description}>Transform any widget into another to try different options, without any design hassle</p>
-      </div> */}
-      
-      <div className={classes.portfolio} ref={portfolioRef}>
-        <div className={`${classes.portfolio_item} ${classes.visible}`}>
-          <div className={classes.portfolio_item_text} >
-            <a href="#" className={classes.portfolio_item_text_link}>Портфолио</a>
-            <h1 className={classes.portfolio_item_text_title}>Kelvin</h1>
-            <p className={classes.portfolio_item_text_text}>Ну кароче там текст вроде как 123 и так далееНу кароче там текст вроде как 123 и так далееНу кароче там текст вроде как 123 и так далее</p>
-          </div>
-          <div className={classes.portfolio_item_img} >
-            <img src={img_1} alt="Project Kelvin" />
-          </div>
-        </div>
+      {/* Вот наша «секция», которая растягивается по высоте */}
+      <div
+        ref={wrapperRef}
+        className={classes.stickyWrapper}
+      >
+        {/* А вот «контент», который будет «прилипать» наверху и ездить по Х */}
+        <div className={classes.overflowHidden}>
+          <div ref={contentRef} className={classes.horizontalContent}>
+            {/* Здесь исходный portfolio-блок */}
+            <div className={classes.portfolio}>
+              <div className={`${classes.portfolio_item}`}>
+                <div className={classes.portfolio_item_text}>
+                  <a href="#" className={classes.portfolio_item_text_link}>Портфолио</a>
+                  <h1 className={classes.portfolio_item_text_title}>Kelvin</h1>
+                  <p className={classes.portfolio_item_text_text}>
+                    Ну кароче там текст вроде как 123 и так далее...
+                  </p>
+                </div>
+                <div className={classes.portfolio_item_img}>
+                  <img src={img_1} alt="Project Kelvin" />
+                </div>
+              </div>
 
-        <div className={classes.portfolio_item}>
-          <div className={classes.portfolio_item_text} >
-            <a href="#" className={classes.portfolio_item_text_link}>Портфолио</a>
-            <h1 className={classes.portfolio_item_text_title}>Ош ТВ</h1>
-            <p className={classes.portfolio_item_text_text}>Ну кароче там текст вроде как 123 и так далееНу кароче там текст вроде как 123 и так далееНу кароче там текст вроде как 123 и так далее</p>
-          </div>
-          <div className={classes.portfolio_item_img} >
-            <img src={img_2} alt="Project Ош ТВ" />
-          </div>
-        </div>
+              <div className={classes.portfolio_item}>
+                <div className={classes.portfolio_item_text}>
+                  <a href="#" className={classes.portfolio_item_text_link}>Портфолио</a>
+                  <h1 className={classes.portfolio_item_text_title}>Ош ТВ</h1>
+                  <p className={classes.portfolio_item_text_text}>
+                    Ну кароче там текст...
+                  </p>
+                </div>
+                <div className={classes.portfolio_item_img}>
+                  <img src={img_2} alt="Project Ош ТВ" />
+                </div>
+              </div>
 
-        <div className={classes.portfolio_item}>
-          <div className={classes.portfolio_item_text} >
-            <a href="#" className={classes.portfolio_item_text_link}>Портфолио</a>
-            <h1 className={classes.portfolio_item_text_title}>Karagay</h1>
-            <p className={classes.portfolio_item_text_text}>Ну кароче там текст вроде как 123 и так далееНу кароче там текст вроде как 123 и так далееНу кароче там текст вроде как 123 и так далее</p>
-          </div>
-          <div className={classes.portfolio_item_img} >
-            <img src={img_3} alt="Project Karagay" />
+              <div className={classes.portfolio_item}>
+                <div className={classes.portfolio_item_text}>
+                  <a href="#" className={classes.portfolio_item_text_link}>Портфолио</a>
+                  <h1 className={classes.portfolio_item_text_title}>Karagay</h1>
+                  <p className={classes.portfolio_item_text_text}>
+                    Ну кароче там текст...
+                  </p>
+                </div>
+                <div className={classes.portfolio_item_img}>
+                  <img src={img_3} alt="Project Karagay" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Portfolio2;
